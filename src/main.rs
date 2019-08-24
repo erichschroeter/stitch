1d7f3a997e6e21866e3db515ba9f138fa461beeb
Erich Schroeter <erichschroeter@gmail.com>
2019-08-24 10:58:11 -0500

    Set up initial command line arg parsing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+extern crate clap;
+use clap::{App, Arg};
+
 fn main() {
-    println!("Hello, world!");
+    let matches = App::new("stitch")
+    .version("1.0")
+    .about("Sitches images together.")
+    .arg(Arg::with_name("x")
+        .help("The X-coordinate.")
+        .short("x")
+        .case_insensitive(true)
+        .default_value("0")
+        .takes_value(true)
+        .multiple(true)
+    )
+    .arg(Arg::with_name("y")
+        .help("The Y-coordinate.")
+        .short("y")
+        .case_insensitive(true)
+        .default_value("0")
+        .takes_value(true)
+        .multiple(true)
+    )
+    .arg(Arg::with_name("IMAGE")
+        .help("The path to the image.")
+        .multiple(true)
+        .min_values(1)
+    )
+    .get_matches();
+    println!("{:?}", matches);
 }
